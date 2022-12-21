48e55205858aa4b05a24f7d6cfb9151d8fece1ea
idamakela <idamakela98@gmail.com>
2022-12-21 18:00:10 +0100

    disabled keyboard when game is done

--- js/app.js
+++ js/app.js
@@ -22,6 +22,8 @@ $(function() {
     let startMessage = "Select a letter!";
     let wrongMessage = "Wrong guess! <br><br> Guess again!";
     let rightMessage = "Right guess! <br><br> Try another one!";
+    let maxWrong = 0;
+    let remainingLetters = 0;
 
     $("#start-button").click(gameFunction);
     $(".cancel-restart").hide();
@@ -32,11 +34,10 @@ $(function() {
         $(".progress").show();
         $(".start-game").hide();
 
-        let answer = randomWord(words);
-        let maxWrong = answer.length + 2;
-        let remainingLetters = answer.length;
+        answer = randomWord(words);
+        maxWrong = answer.length + 2;
+        remainingLetters = answer.length;
 
-        //ERROR: does not wanna change when right letter input
         for (let i = 0; i < answer.length; i++) {
             answerLetters[i]  = "_";
         }
@@ -44,121 +45,59 @@ $(function() {
         $(".answer").append(answerLetters.join(" "));
         $(".progress").html("<h3>" + startMessage + "<br><br> You have " + maxWrong + " lives left." + "</h3>")
 
-
-        //keyboard, userinput and test user input
         createKeyboard(alphabet, letterButtons);
 
-        $(".keyboard button").on("click", function() {
-            $(this).attr("disabled", "true");
-            userGuessed.push($(this).text());
-            userInput = $(this).text();
-
-
-    
-            //game loop
-            //while (remainingLetters > 0) {       
-    
-                if (answer.includes(userInput)) {                  
-                    for (let j = 0; j < answer.length; j++) {
-                        if (answer[j] === userInput) {
-                            answerLetters[j] = userInput;
-                            //$(".answer").append(answerLetters[j])
-                            $(".answer").append(userInput);
-                        }
-                    }
-
-                    console.log(answerLetters + typeof(answerLetters))
-
-
-                    remainingLetters--;                
-                    $(".progress").html("<h3>" + rightMessage + "<br><br> You have " + maxWrong + " lives left." + "</h3>")
-                } else {
-                    maxWrong--;
-                    $(".progress").html("<h3>" + wrongMessage + "<br><br> You have " + maxWrong + " lives left." + "</h3>")
-                }
-                if (maxWrong === 0) {
-                    $(".progress").html("<h3>You Lost! <br><br> The answer was: " + answer.toUpperCase() + "<h3>")
-                    userStatus = "lost";
-                    //break; 
-                } 
-            //}
-    
-            if (remainingLetters === 0) {
-                $(".progress").html("<h3>Congratulations! <br><br> You have won the game! <br><br> The answer was: " + answer.toUpperCase() + "<h3>");
-                userStatus = "won";
-            }
-        });
+        $(".keyboard button").on("click",  testUserInput);
         
-
-        //cancel and restart button
         $("#cancel-button").click(function() {
             location.reload(true);
         });
 
-    }
+        $("#restart-button").click(function() {
 
+        });
+
+    }
 
     function testUserInput() {
         $(this).attr("disabled", "true");
         userGuessed.push($(this).text());
-        
         userInput = $(this).text();
 
-        let remainingLetters = answer.length;
-
-        console.log(userInput)
-
-
-        //game loop
-        while (remainingLetters > 0) {    
-
-            //ERROR: userinput is five but should not have a value
-            console.log(userInput + typeof(userInput))
-
-            console.log(answer)
-
-
-            if (answer.toUpperCase().includes(userInput)) {
-
-                for (let j = 0; j < answer.length; j++) {
-
-                    if (answer[j] === userInput) {
-                        answerLetters[j] = userInput;
-                    } else {
-                        console.log("BROKEN")
-                    }
+        if (answer.includes(userInput)) {                  
+            for (let j = 0; j < answer.length; j++) {
+                if (answer[j] === userInput) {
+                    answerLetters[j] = userInput;
+                    $(".answer").text(answerLetters.join(" "));
                 }
-
-
-                remainingLetters--;                
-                $(".progress").html("<h3>" + rightMessage + "<br><br> You have " + maxWrong + " lives left." + "</h3>")
-
-            } else {
-                maxWrong--;
-                $(".progress").html("<h3>" + wrongMessage + "<br><br> You have " + maxWrong + " lives left." + "</h3>")
             }
-
-            if (maxWrong === 0) {
-                $(".progress").html("<h3>You Lost! <br><br> The answer was: " + answer.toUpperCase() + "<h3>")
-                userStatus = "lost";
-                break; 
-            } 
+            remainingLetters--;
+            $(".progress").html("<h3>" + rightMessage + "<br><br> You have " + maxWrong + " lives left." + "</h3>")
+        } else {
+            maxWrong--;
+            $(".progress").html("<h3>" + wrongMessage + "<br><br> You have " + maxWrong + " lives left." + "</h3>")
         }
+        
+        if (maxWrong === 0) {
+            $(".progress").html("<h3>You Lost! <br><br> The answer was: " + answer.toUpperCase() + "<h3>")
+            $(".keyboard button").attr("disabled", "true")
+            userStatus = "lost";
+        } 
 
         if (remainingLetters === 0) {
             $(".progress").html("<h3>Congratulations! <br><br> You have won the game! <br><br> The answer was: " + answer.toUpperCase() + "<h3>");
+            $(".keyboard button").attr("disabled", "true")
             userStatus = "won";
         }
+
     }
-
-
 
     function createKeyboard(targetArray, newArray) {
         isThisArray(targetArray);
         isThisArray(newArray);
 
         for(i = 0; i < targetArray.length; i++) {
-            newArray.push('<button class=' + '"' + targetArray[i] + '"' + '>' + targetArray[i] + '</button>');
+            newArray.push("<button>" + targetArray[i] + "</button>");
         }
 
         $(".keyboard").html(newArray.join(""));
@@ -174,9 +113,4 @@ $(function() {
     function randomWord(targetArray) {
         return targetArray[Math.floor(Math.random() * targetArray.length)];
     }
-
-
-
-
-
 });
